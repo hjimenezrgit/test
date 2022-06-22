31ce4ad4600335513e226df2564d0ebac8e12b29
hjimenezrgit <107966959+hjimenezrgit@users.noreply.github.com>
2022-06-22 17:16:39 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ v1 = client.CoreV1Api()
 
 pod_list = v1.list_namespaced_pod("default")
 for pod in pod_list.items:
-    print("%s\t%s\t%s" % (pod.metadata.name, 
+    return("%s\t%s\t%s" % (pod.metadata.name, 
                           pod.status.phase,
                           pod.status.pod_ip))
 
