6e06e83e3a3d4271848c89055c656d42fc08f7c0
hjimenezrgit <107966959+hjimenezrgit@users.noreply.github.com>
2022-06-22 15:55:48 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from kubernetes.client.rest import ApiException
+from kubernetes import client, config
 
 app = Flask(__name__)
 
@@ -6,6 +8,20 @@ app = Flask(__name__)
 def hello():
     return "Hello World!!!"
 
+class KubernetesService:
+    def __init__(self):
+        super().__init__()
+        load_config()
 
+pod_name = "devops-arkon"
+try:
+    api_instance = client.CoreV1Api()
+    api_response = api_instance.read_namespaced_pod_log(name=pod_name, namespace='default')
+    print(api_response)
+except ApiException as e:
+    print('Found exception in reading the logs')
+    
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
+
