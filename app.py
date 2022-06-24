62b6ef0d2fa13063e662319107c914e4ea339361
hjimenezrgit <107966959+hjimenezrgit@users.noreply.github.com>
2022-06-24 12:25:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,56 @@
 import os
-from flask import Flask
+import time
+from flask import Flask ,request
 from kubernetes.client.rest import ApiException
 from kubernetes import client, config
+import logging
 
 app = Flask(__name__)
+#logging.basicConfig(filename='record.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+logging.basicConfig(filename='/opt/nfs/record.log', level=logging.INFO, format=f' %(threadName)s : %(message)s')
+ 
+#file1 = open('record.log', 'r')
+#Lines = file1.readlines()
 
-config.load_kube_config(
-    os.path.join(os.environ["HOME"], '/opt/app/config'))
+#count = 0
+#poc = 0
+#string1 = 'POST'
 
-v1 = client.CoreV1Api()
+#for line in Lines:
+#    count += 1
+#    if string1 in line:
+#        poc += 1
 
-pod_list = v1.list_namespaced_pod("default")
+#print(poc)  
+global poc
+poc = 0
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return "Hello World!!!"
-for pod in pod_list.items:
-    print("%s\t%s\t%s" % (pod.metadata.name, 
-                          pod.status.phase,
-                          pod.status.pod_ip))
+    app.logger.info('Info level log')
+    file1 = open('/opt/nfs/record.log', 'r')
+    Lines = file1.readlines()
+    count = 0
+    string1 = 'POST'
+    if request.method == 'POST':
+        globals()['poc'] = 0
+        for line in Lines:
+            count += 1
+            if string1 in line:
+                globals()['poc'] += 1    
+        return "Hello World!!!"
+    else:
+        return str(globals()['poc'])    
 
+    #
 
+#def log_request_info():
+#    app.logger.debug('Headers: %s', request.headers)
+#    app.logger.debug('Body: %s', request.get_data())
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
-    
 
+    
